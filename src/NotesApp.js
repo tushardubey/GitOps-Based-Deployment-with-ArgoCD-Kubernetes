62e858d0ecaad36f19f8389a8688740f1e54a5b5
Tushar Dubey <dubey.tushar9060@gmail.com>
2025-03-26 18:55:02 +0530

    updated NotesApp.js

--- src/NotesApp.js
+++ src/NotesApp.js
@@ -122,7 +122,7 @@ function NotesApp() {
 
     return (
         <div className="notes-app">
-            <h1>MY NOTES</h1>
+            <h1>MY NOTESss</h1>
             <button onClick={handleAddNoteClick} className="add-note-button">
                 Add Notes
             </button>
